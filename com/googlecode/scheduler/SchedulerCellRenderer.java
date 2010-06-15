6f80b217505566f92210b9ba78d82bb98fe9120f
spyros@valor.gr <spyros@valor.gr@487c8876-59a6-a105-3a2e-643a8f4a94df>
2010-06-15 11:52:09 +0000

    Refactor

--- com/googlecode/scheduler/SchedulerCellRenderer.java
+++ com/googlecode/scheduler/SchedulerCellRenderer.java
@@ -8,14 +8,11 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
 import java.awt.Image;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JTable;
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableCellRenderer;
-import sun.beans.editors.ColorEditor;
 
 /**
  *
